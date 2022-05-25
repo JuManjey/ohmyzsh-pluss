06f0190a24604da23fd4590e8a8c32069864f999
JuManji <opghld@gmail.com>
2022-05-25 19:15:01 +0300

    Update ohmyzsh_x2go.sh

--- ohmyzsh_x2go.sh
+++ ohmyzsh_x2go.sh
@@ -9,27 +9,10 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 echo " DONT FORGET - zsh-autosuggestions zsh-syntax-highlighting zsh-autosuggestions zsh-completions zsh-history-substring-search"
-echo "duellj"
+
 nano .zshrcexec
-omz plugin load history-substring-search keychain magic-enter npx perl pip please poetry penv edis-cli rust safe-paste 
-omz plugin load supervisor sublime symfony textmate thor universalarchive yii ansible arcanist autoenv bundler 
-omz plugin load command-not-found composer copyfile docker-machine encode64 fancy-ctrl-z firewalld
-omz plugin load git it-hubflow 
+echo "duellj"
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-autosuggestions zsh-completions zsh-history-substring-search vagrant genpass gpg-agent kate lpass python shell-proxy systemadmin timer transfer virtualenvwrapper web-search zsh-navigation-tools gitignore git-flow-avh screen systemd vagrant-prompt git-hubflow history-substring-search keychain magic-enter perl pip please poetry pyenv rails redis-cli rust safe-paste supervisor sublime symfony textmate thor universalarchive yii ansible arcanist autoenv bundler command-not-found composer copyfile docker-machine encode64 fancy-ctrl-z firewalld forklift fzf git-extras gitfast glassfish golang grails gulp heroku httpie ipfs kitchen knife_ssh last-working-dir mysql-macports operator-sdk nvm pass pipenvpostgres powify pylint rake rbenv repo rsync ssh-agent sublime-merge svn-fast-info symfony2 taskwarrior terraform thefuck tmux-cssh torrent ubuntu urltools vault virtualenv vscode xcode zbell zsh-interactive-cd pipenv postgres)
 
-omz plugin load lassfish golang grails  gulp heroku hitchhiker httpie ipfs kitchen knife_ssh last-working-dir mysql-macports operator-sdk 
-omz plugin load pass pipenv postgres powify pylint rake rbenv repo rsync ssh-agent sublime-merge svn-fast-info symfony2 taskwarrior 
-omz plugin load terraform thefuck tmux-cssh torrent ubuntu urltools vault virtualenv vscode xcode zbell zsh-interactive-cd genpass gpg-agent 
-omz plugin load lpass python shell-proxy systemadmin timer transfer virtualenvwrapper web-search zsh-navigation-tools gitignore 
-omz plugin load screen systemd vagrant-prompt
-
-omz plugin enable history-substring-search keychain magic-enter npx perl pip please poetry rust safe-paste 
-omz plugin enable supervisor sublime symfony textmate thor universalarchive yii ansible arcanist autoenv bundler 
-omz plugin enable command-not-found composer copyfile docker-machine encode64 fancy-ctrl-z firewalld
-
-omz plugin enable git git-hubflow 
-omz plugin enable lassfish golang grails  gulp heroku hitchhiker httpie ipfs kitchen knife_ssh last-working-dir mysql-macports operator-sdk 
-omz plugin enable pass pipenv postgres powify pylint rake rbenv repo rsync ssh-agent sublime-merge svn-fast-info symfony2 taskwarrior 
-omz plugin enable terraform thefuck tmux-cssh torrent ubuntu urltools vault virtualenv vscode xcode zbell zsh-interactive-cd genpass gpg-agent 
-omz plugin enable lpass python shell-proxy systemadmin timer transfer virtualenvwrapper web-search zsh-navigation-tools gitignore 
-omz plugin enable screen systemd vagrant-prompt
+omz update
 source .zshrc
